e4f09027b114ce4e5ef46e98e7678b773b755cb7
GabyG <gab.gomez032@gmail.com>
2021-10-07 07:08:42 -0500

    Parte 1.2

--- Archivo.py
+++ Archivo.py
@@ -1,12 +1,25 @@
-from os import read
 from tkinter import filedialog
 
 #Agregar el archivo de texto
 def add_File():
     new_File = filedialog.askopenfilename(title="Add File", initialdir="c:/desktop", filetypes=(("txt files","*.txt"),("todos los archivos","*.*")))
     if new_File != '':
-        file_Automaton = open(new_File,"r")
-        new_text = file_Automaton.read()
-        file_Automaton.close()
-        print(new_text)
-    return new_text
+        with open(new_File) as n_files:
+            lines_saved = n_files.readlines()
+        n_files.close()
+
+        data_Saved = []
+        for lines_read in lines_saved:
+            lines_read = lines_read.replace("\n","")
+            lines_read = lines_read.replace("{","")
+            lines_read = lines_read.replace("}","")
+            lines_read = lines_read.replace("=","")
+            lines_read = lines_read.replace(" ","")
+            lines_read = lines_read.replace(",","")
+            data_Saved.append(lines_read)
+
+    return data_Saved
+
+def compare_Automaton():
+    pass
+
--- AutomataF.py
+++ AutomataF.py
@@ -1,10 +1,12 @@
 from tkinter import Tk, ttk
 from Archivo import add_File
+import pandas as pd
 
 class Automata: 
     
     def __init__(self):
-        self.add_FilesW()
+        file_saves = self.add_FilesW()
+        self.analyze_File(file_saves)
 
     #Abrir la ventana para subir archivo
     def add_FilesW(self):
@@ -12,10 +14,47 @@ class Automata:
         self.windows.geometry("300x100")
         self.windows.title("Automata Finito")
         self.windows.resizable(0,0)
-
-        btn_File = ttk.Button(self.windows, text="Add File", command=lambda: add_File())
-        btn_File.place(relx=0.36, rely=0.30)
-
+        file_save = add_File()
         self.windows.mainloop()
+        return file_save
+    
+    def analyze_File(self,file_saved):
+        
+        data_Saved = list(file_saved)
 
+        #Hace una busqueda de cada caracter 
+        dat_Q = [s for s in data_Saved if "Q" in s]
+        dat_S = [s for s in data_Saved if "S" in s]
+        dat_q0 = [s for s in data_Saved if "q0" in s]
+        dat_F = [s for s in data_Saved if "F" in s]
+        dat_D = [s for s in data_Saved if "D" in s]
+
+        #convertir a cadenas
+        Str_Q = "".join(dat_Q)
+        Str_S = "".join(dat_S)
+        Str_q0 = "".join(dat_q0)
+        Str_F = "".join(dat_F)
+        Str_D = "".join(dat_D)
+
+        #Convertir a listas
+        row_Q = list(Str_Q)
+        row_S = list(Str_S)
+        row_q0 = list(Str_q0)
+        row_F = list(Str_F)
+        row_D = list(Str_D)
+
+        # Eliminar Letras
+        row_Q.pop(0)
+        row_S.pop(0)
+        row_q0.pop(0)
+        row_F.pop(0)
+        row_D.pop(0)
+       
+        print("Nueva Q: ", row_Q)
+        print("Nueva S: ", row_S)
+        print("Nueva q0: ", row_q0)
+        print("Nueva F: ", row_F)
+        print("Nueva D: ", "".join(row_D))
+
+      
 app = Automata()
