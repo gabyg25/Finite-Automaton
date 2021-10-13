84adefa02aa57c455a67d89504fde623f9a6543c
GabyG <gab.gomez032@gmail.com>
2021-10-13 05:00:59 -0500

    version2

--- Archivo.py
+++ Archivo.py
@@ -1,26 +1,86 @@
-from tkinter import filedialog
+import os
+from tkinter import filedialog, messagebox
 
-#Agregar el archivo de texto
+# Agregar el archivo de texto
 def add_File():
-    new_File = filedialog.askopenfilename(title="Add File", initialdir="c:/desktop", filetypes=(("txt files","*.txt"),("todos los archivos","*.*")))
+    states_File = False
+    new_File = filedialog.askopenfilename(
+        title="Add File", initialdir="c:/desktop", filetypes=(("txt files", "*.txt"), ("todos los archivos", "*.*")))
     if new_File != '':
-        with open(new_File) as n_files:
-            lines_saved = n_files.readlines()
-        n_files.close()
+        new_extension = os.path.splitext(new_File)
+        if new_extension[1] == ".txt":
+            with open(new_File) as n_files:
+                lines_saved = n_files.readlines()
+                for read_Lines in lines_saved:
+                    read_Lines = read_Lines.replace("{", '').replace("}",'')
+                    read_Lines = read_Lines.replace("\t", '')
+                    if read_Lines[0] == 'Q':
+                        read_Lines = read_Lines.replace("Q=", '').replace("\n",'')
+                        add_States(read_Lines)
+                    if read_Lines[0] == 'S':
+                        read_Lines = read_Lines.replace("S=", '').replace("\n",'')
+                        add_Alphabet(read_Lines)
+                    if read_Lines[0] == 'q':
+                        read_Lines = read_Lines.replace("q0=", '').replace("\n",'')
+                        add_InitialState(read_Lines)
+                    if read_Lines[0] == 'F':
+                        read_Lines = read_Lines.replace("F=", '').replace("\n",'')
+                        add_FinalState(read_Lines)
+                    if read_Lines[0] == 'D':
+                        read_Lines = read_Lines.replace("D=",'').replace("\n",'')
+                        add_Transitions(read_Lines)
+            states_File = True
+        else:
+            messagebox.showwarning("Alerta", "El archivo no es un Txt")
+    return states_File
 
-        data_Saved = []
-        for lines_read in lines_saved:
-            lines_read = lines_read.replace("\n","")
-            lines_read = lines_read.replace("\t","")
-            lines_read = lines_read.replace("{","")
-            lines_read = lines_read.replace("}","")
-            lines_read = lines_read.replace("=","")
-            lines_read = lines_read.replace(" ","")
+#Definir los conjuntos
+new_States = set()
+new_Alphabet = set()
+new_Initial = set()
+new_Final = set()
+new_Transitions = dict()
 
-            data_Saved.append(lines_read)
+def add_States(lines):
+    states = lines.split(",")
+    for state in states:
+        if state !="" or state !=" ":
+            new_States.add(state)
 
-    return data_Saved
+def add_Alphabet(lines):
+    alphabets = lines.split(",")
+    for alphabet in alphabets:
+        if alphabet !="" or alphabet !=" ":
+            new_Alphabet.add(alphabet)
 
-def compare_Automaton(list_transition):
+def add_InitialState(lines):
+    initial_State = lines.split(",")
+    for initial in initial_State:
+        if initial !="" or initial !=" ":
+            new_Initial.add(initial)
+
+def add_FinalState(lines):
+    final_State = lines.split(",")
+    for final in final_State:
+        if final !="" or final !=" ":
+            new_Final.add(final)
+
+def add_Transitions(lines):
     pass
 
+#-------- Obtener y enviar datos ---
+def send_States():
+    return new_States
+
+def send_Alphabet():
+    return new_Alphabet
+
+def send_InitialState():
+    return new_Initial
+
+def send_FinalState():
+    return new_Final
+
+def send_Transitions():
+    return new_Transitions
+    
--- AutomataF.py
+++ AutomataF.py
@@ -1,69 +1,23 @@
-from tkinter import Tk, ttk
-from Archivo import add_File
-import pandas as pd
-
+from tkinter import Tk, ttk, messagebox
+from Archivo import *
 
 class Automata:
 
     def __init__(self):
-        self.add_FilesW()
-
-    # Abrir la ventana para subir archivo
-    def add_FilesW(self):
         self.windows = Tk()
-        self.windows.geometry("300x100")
+        self.windows.geometry("300x150")
         self.windows.title("Automata Finito")
         self.windows.resizable(0, 0)
-        file_save = add_File()
-        self.analyze_File(file_save)
-
+        data = add_File()
+        if data is True:
+            self.main_Interface()
+        else:
+            messagebox.showerror("Error", "No Cargo Ningún Archivo")
+            self.windows.destroy()
         self.windows.mainloop()
-        
-
-    def analyze_File(self, file_saved):
-
-        data_Saved = list(file_saved)
-
-        # Hace una busqueda de cada caracter
-        dat_Q = [s for s in data_Saved if "Q" in s]
-        dat_S = [s for s in data_Saved if "S" in s]
-        dat_q0 = [s for s in data_Saved if "q_0" in s]
-        dat_F = [s for s in data_Saved if "F" in s]
-        dat_D = [s for s in data_Saved if "D" in s]
-
-        # convertir a cadenas
-        Str_Q = "".join(dat_Q)
-        Str_S = "".join(dat_S)
-        Str_q0 = "".join(dat_q0)
-        Str_F = "".join(dat_F)
-        Str_D = "".join(dat_D)
-
-        # Eliminar letras
-        dat_Q = Str_Q.replace("Q", "")
-        dat_S = Str_S.replace("S", "")
-        dat_q0 = Str_q0.replace("q_0", "")
-        dat_F = Str_F.replace("F", "")
-        dat_D = Str_D.replace("D", "")
-
-        #Convertir de nuevo a lista
-        row_Q = dat_Q.split(',')
-        row_S = dat_S.split(',')
-        row_q0 = dat_q0.split(',')
-        row_F = dat_F.split(',')
-        row_D = dat_D.split(',')
-        
-        # row_D2 = [row_D[i:i + 3] for i in range(0, len(row_D), 3)]
-
-        print("Q: ", row_Q)
-        print("S: ", row_S)
-        print("q0:",row_q0)
-        print("F: ", row_F)
-        print("D: ", row_D)
-
+ 
+    def main_Interface(self):
         self.windows.destroy()
-        # self.main_Interface(row_Q, row_S, row_q0, row_F, row_D2)
-
-    def main_Interface(self, Q, S, q_0, F, D):
         main_windows = Tk()
         main_windows.geometry("300x200")
         main_windows.title("AFN/AFD/")
@@ -73,11 +27,22 @@ class Automata:
         ttk.Label(main_windows,text="Cadena: ").place(relx=0.15,rely=0.35)
         add_String = ttk.Entry(main_windows)
         add_String.place(relx=0.35,rely=0.35)
-        Btn_Review = ttk.Button(main_windows, text="Evaluar Cadena",command=lambda:"#")
+        Btn_Review = ttk.Button(main_windows, text="Evaluar Cadena",command=lambda:self.analyze_File())
         Btn_Review.place(relx=0.35, rely=0.55)
         main_windows.mainloop()
     
-    
+    def analyze_File(self):
 
+        states = send_States()
+        alphabet = send_Alphabet()
+        initial = send_InitialState()
+        final = send_FinalState()
+        trans = send_Transitions()
 
-app = Automata()
+        print("Q: ",states)
+        print("S:", alphabet)
+        print("q0:", initial)
+        print("F", final)
+        print("D:", trans)
+
+Automata()
